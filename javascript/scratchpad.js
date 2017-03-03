0728b5ebbdc8465205ea3e10fe52b614b02bbbc3
Ally Spencer <allybspencer@gmail.com>
2017-03-03 01:18:39 +0000

    Added Trib sequence

--- javascript/scratchpad.js
+++ javascript/scratchpad.js
@@ -33,3 +33,40 @@ var fibonacci = function (n, node) {
   node.appendChild(fibTree.html)
 }
 fibonacci(6, document.querySelector('.red'))
+
+function pellSequence(n) {
+  var value;
+  var div = document.createElement('div');
+    if (n === 0) {
+      value = 0;
+        var p = document.createElement('p');
+        p.textContent = 'Fib(' + n + ') = ' + value;
+        div.appendChild(p)
+    } 
+    else if (n === 1) {
+      value = 1;
+        var p = document.createElement('p');
+        p.textContent = 'Fib(' + n + ') = ' + value;
+        div.appendChild(p)
+    }
+  else {
+    var left = pellSequence(n - 1);
+    var right = pellSequence(n - 2);
+   
+    value = 2 * left.value + right.value;
+    var p = document.createElement('p');
+    p.textContent = 'Fib(' + n + ') = ' + value;
+    div.appendChild(p);
+    
+    div.appendChild(left.html);
+    div.appendChild(right.html);
+  }
+  return { 'value': value, 'html': div };
+}
+
+var pell = function (n, node) {
+  var pellTree = fibSequence(n)
+  node.appendChild(pellTree.html)
+}
+pell(6, document.querySelector('.yellow'))
+
